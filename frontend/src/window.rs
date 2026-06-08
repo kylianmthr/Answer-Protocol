67f0ace77397dd91f1caf342f772e551340287f8
theosykas <theosykas@icloud.com>
2026-06-08 21:46:53 +0200

    borrow?_fixing

--- frontend/src/window.rs
+++ frontend/src/window.rs
@@ -35,6 +35,27 @@ struct GamePage {
 	item: String
 }
 
+impl MyTap {
+	fn draw_field_log(&mut self, ui: &mut egui::Ui, login_page: &mut LoginPage) {
+		ui.vertical_centered(|ui| {
+						ui.add_space(250.0);
+
+						let style_field = ui.style_mut();
+
+						style_field.visuals.widgets.inactive.bg_fill = egui::Color32::WHITE;
+						style_field.override_font_id = Some(
+							egui::FontId::proportional(24.0));
+
+						ui.colored_label(egui::Color32::WHITE, "username:");
+						ui.text_edit_singleline(&mut login_page.username);
+
+						ui.add_space(42.0);
+						ui.colored_label(egui::Color32::WHITE, "address serveur:");
+						ui.text_edit_singleline(&mut login_page.serveur_adrr);
+		});
+	}
+}
+
 // apply contrat (App) on MyTap
 impl eframe::App for MyTap{
 	// modify (mut) once per frame
@@ -51,22 +72,8 @@ impl eframe::App for MyTap{
 			egui::Image::new(image_log_bg).paint_at(ui, get_rect_screen);
 			match &mut self.screen {
 				Screen::LoginView(login_page) => {
-					ui.vertical_centered(|ui| {
-						ui.add_space(250.0);
 
-						let style_field = ui.style_mut();
-
-						style_field.visuals.widgets.inactive.bg_fill = egui::Color32::WHITE;
-						style_field.override_font_id = Some(
-							egui::FontId::proportional(24.0));
-
-						ui.colored_label(egui::Color32::WHITE, "username:");
-						ui.text_edit_singleline(&mut login_page.username);
-
-						ui.add_space(42.0);
-						ui.colored_label(egui::Color32::WHITE, "address serveur:");
-						ui.text_edit_singleline(&mut login_page.serveur_adrr);
-					});
+						self.draw_field_log(ui, login_page);
 				}
 				Screen::GameView(game_page) => {
 					todo!("atrr of game screen")  // c'est genial todo
