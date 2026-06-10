26833aed7cd8f9d2fe5f3b897ae5c9c6af252257
Kylian Mathurin <96012700+kylianmthr@users.noreply.github.com>
2026-06-10 13:33:10 +0200

    Fix: add broadcast function

---
+++ backend/src/broadcast.rs
@@ -0,0 +1,11 @@
+use crate::state::SharedState;
+use std::sync::Arc;
+
+pub async fn broadcast_room(room_id: &str, message: &str, state: Arc<SharedState>) {
+    let players = state.players.lock().await;
+    players.iter().for_each(|(_, player)| {
+        if player.room == room_id {
+            let _ = player.tx.send(message.to_string());
+        }
+    });
+}
