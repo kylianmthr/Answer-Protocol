714bb042e3de49d4e13d7bd303656e2d43b57b08
theosykas <theosykas@icloud.com>
2026-06-11 10:48:46 +0200

    Feature: Add new font and changed some style

--- frontend/src/window.rs
+++ frontend/src/window.rs
@@ -1,7 +1,9 @@
+use std::sync::Arc;
+
 use crate::auth::auth;
 use crate::parser::ServerMessage;
 use eframe::egui;
-use egui::Ui;
+use egui::{FontData, FontDefinitions, FontFamily, Ui};
 use egui_notify::Toasts;
 
 // init screen egui(GUI)
@@ -60,41 +62,83 @@ struct GamePage {
     item: String,
 }
 
+// cas ou tu veux ajouter des font cas specifique
+// pub struct Font {
+// 	undertale_font: String,
+// }
+
+pub fn font_style(egui_ctx: &egui::Context) {
+	let mut undertale_font = FontDefinitions::default();
+
+	undertale_font.font_data.insert("undertale_font".to_owned(),
+		Arc::new(FontData::from_static(include_bytes!("../font/undertale_font.ttf"))));
+
+	undertale_font.families.insert(FontFamily::Name("undertale_font".into()),
+		vec!["undertale_font".to_owned()],
+	);
+
+	//font priority projet
+	undertale_font.families.get_mut(&FontFamily::Proportional)
+	.unwrap()
+	.insert(0, "undertale_font".to_owned());
+
+	// security_font
+	undertale_font.families.get_mut(&FontFamily::Monospace)
+	.unwrap()
+	.push(("undertale_font").to_owned());
+
+	egui_ctx.set_fonts(undertale_font);
+}
+
 impl MyTap {
     fn draw_field_log(ui: &mut egui::Ui, login_page: &mut LoginPage) {
         ui.vertical_centered(|ui| {
             ui.add_space(250.0);
 
-            let style_field = ui.style_mut();
+            ui.scope(|ui|{
+				let style_field = ui.style_mut();
+				let rounding_field = egui::CornerRadius::same(10_u8);
 
-            style_field.visuals.widgets.inactive.bg_fill = egui::Color32::WHITE;
-            style_field.override_font_id = Some(egui::FontId::proportional(24.0));
+				style_field.visuals.extreme_bg_color = egui::Color32::WHITE;
+				style_field.visuals.override_text_color = Some(egui::Color32::BLACK);
 
-            ui.colored_label(egui::Color32::WHITE, "username:");
-            ui.text_edit_singleline(&mut login_page.username);
+				style_field.visuals.widgets.active.corner_radius = rounding_field;
+				style_field.visuals.widgets.hovered.corner_radius = rounding_field;
+				style_field.visuals.widgets.inactive.corner_radius = rounding_field;
+				style_field.override_font_id = Some(egui::FontId::proportional(24.0_f32));
+				style_field.visuals.widgets.inactive.bg_fill = egui::Color32::WHITE;
+
+				ui.add(egui::TextEdit::singleline(&mut login_page.username).
+					hint_text("Username:")
+					.font(egui::FontId::new(20.0_f32,
+					egui::FontFamily::Name("undertale_font".into())))
+				);
+			});
 
             ui.add_space(42.0);
-            if ui.button("Login").clicked() {
-                login_page
-                    .tx_outgoing
-                    .send(format!("CONNECT {}", login_page.username))
-                    .unwrap();
-                login_page.waiting_res = true;
-                //match auth(
-                //    &login_page.rx_incoming,
-                //    &login_page.tx_outgoing,
-                //    login_page.username.clone(),
-                //) {
-                //    Ok(_) => {
-                //        login_page.toasts.success("Login successful".to_string());
-                //        println!("Login successful");
-                //    }
-                //    Err(e) => {
-                //        println!("Login failed: {}", e);
-                //        login_page.toasts.error(format!("Login failed: {}", e));
-                //    }
-                //}
-            }
+			ui.scope(|ui| {
+				if ui.button("Login").clicked() {
+					login_page
+						.tx_outgoing
+						.send(format!("CONNECT {}", login_page.username))
+						.unwrap();
+					login_page.waiting_res = true;
+					//match auth(
+					//    &login_page.rx_incoming,
+					//    &login_page.tx_outgoing,
+					//    login_page.username.clone(),
+					//) {
+					//    Ok(_) => {
+					//        login_page.toasts.success("Login successful".to_string());
+					//        println!("Login successful");
+					//    }
+					//    Err(e) => {
+					//        println!("Login failed: {}", e);
+					//        login_page.toasts.error(format!("Login failed: {}", e));
+					//    }
+					//}
+				}
+			});
         });
     }
 }
@@ -108,7 +152,7 @@ impl eframe::App for MyTap {
         egui::CentralPanel::default()
             .frame(remove_border_bg)
             .show(ctx, |ui| {
-                let image_log_bg = egui::include_image!("../asset_manager/log_bg_1.png");
+				let image_log_bg = egui::include_image!("../asset_manager/asset_log.jpeg");
                 let get_rect_screen = ui.max_rect(); // window_size
 
                 egui::Image::new(image_log_bg).paint_at(ui, get_rect_screen);
