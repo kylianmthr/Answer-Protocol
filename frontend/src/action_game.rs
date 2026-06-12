7479421b6e592b48a33c5e6bb1ec0fe86ab348a8
theosykas <theosykas@icloud.com>
2026-06-12 12:16:07 +0200

    impl_whileGame

--- frontend/src/action_game.rs
+++ frontend/src/action_game.rs
@@ -3,7 +3,7 @@ use eframe::egui;
 #[derive(Clone)]
 pub enum StateAction {
 	MOVE,
-	// ATTACK,
+	ATTACK,
 }
 
 pub struct ComandeButton {
@@ -22,7 +22,7 @@ impl ComandeButton {
 	pub fn draw_click_game(&mut self, ui: &mut egui::Ui, tx_outcomming: &std::sync::mpsc::Sender<String>) {
 		let rect_screen = ui.max_rect();
 		let pos_bottom = egui::pos2(rect_screen.center().x, rect_screen.max.y - (30.0));
-		let rect_put = egui::Rect::from_center_size(pos_bottom, egui::Vec2::new(50.0, 40.0));
+		let rect_put = egui::Rect::from_center_size(pos_bottom, egui::Vec2::new(250.0, 40.0));
 
 		match self.current_action.clone() {
 			None => {
@@ -31,16 +31,13 @@ impl ComandeButton {
 						if Self::click_button(ui, "MOVE") {
 							self.current_action = Some(StateAction::MOVE);
 						}
-						// if Self::click_button(ui, "ATTACK") {
-						// 	self.current_action = Some(StateAction::ATTACK);
-						// }
+						if Self::click_button(ui, "ATTACK") {
+							self.current_action = Some(StateAction::ATTACK);
+						}
 					}).response
 				});
 			}
 			Some(StateAction::MOVE) => {
-					// egui::Panel::bottom("menu_direction")
-					// .min_size(42.0_f32)
-					// .show_inside(ui, |ui| {
 					ui.put(rect_put, |ui: &mut egui::Ui| {
 						ui.horizontal(|ui| {
 						if Self::click_button(ui, "NORTH") {
@@ -62,10 +59,10 @@ impl ComandeButton {
 					}).response
 			});
 		}
-			// Some(StateAction::ATTACK) => {
-			// 	tx_outcomming.send("ATTACK".to_string()).unwrap();
-			// 	self.current_action = None;
-			// };
+		Some(StateAction::ATTACK) => {
+				tx_outcomming.send("ATTACK".to_string()).unwrap();
+				self.current_action = None;
+			}
 		}
 	}
 }
--- frontend/src/window.rs
+++ frontend/src/window.rs
@@ -2,16 +2,19 @@
 // use crate::auth::auth;
 use crate::{action_game::ComandeButton,
 	game_mod::{state_mod::{GameScreen, StateRoom}}};
-	use egui::{FontData, FontDefinitions, FontFamily, Ui};
-	use crate::parser::ServerMessage;
-	use egui_notify::Toasts;
-	use std::sync::Arc;
-	use eframe::egui;
+use egui::{FontData, FontDefinitions, FontFamily, Ui};
+use crate::parser::ServerMessage;
+use egui_notify::Toasts;
+use std::sync::Arc;
+use eframe::egui;
+
 
 // init screen egui(GUI)
 //app::MyTape
 pub struct MyTap {
     screen: Screen,
+    pub rx_incoming: std::sync::mpsc::Receiver<ServerMessage>,
+    pub tx_outgoing: std::sync::mpsc::Sender<String>,
 }
 
 // default start program into login page
@@ -23,8 +26,10 @@ impl MyTap {
         tx_outgoing: std::sync::mpsc::Sender<String>,
     ) -> Self {
         Self {
-            screen: Screen::LoginView(LoginPage::new(rx_incoming, tx_outgoing)),
-        }
+            screen: Screen::LoginView(LoginPage::new()),
+			rx_incoming,
+			tx_outgoing
+		}
     }
 }
 
@@ -34,23 +39,22 @@ enum Screen {
     GameView(GameScreen),
 }
 
+#[derive(Default)]
 struct LoginPage {
     username: String,
-    rx_incoming: std::sync::mpsc::Receiver<ServerMessage>,
-    tx_outgoing: std::sync::mpsc::Sender<String>,
     toasts: Toasts,
     waiting_res: bool,
 }
 
+struct GamePage {
+    username: String,
+}
+
 impl LoginPage {
     pub fn new(
-        rx_incoming: std::sync::mpsc::Receiver<ServerMessage>,
-        tx_outgoing: std::sync::mpsc::Sender<String>,
     ) -> Self {
         Self {
             username: String::new(),
-            rx_incoming,
-            tx_outgoing,
             toasts: Toasts::default(),
             waiting_res: false,
         }
@@ -95,7 +99,7 @@ pub fn font_style(egui_ctx: &egui::Context) {
 }
 
 impl MyTap {
-    fn draw_field_log(ui: &mut egui::Ui, login_page: &mut LoginPage) {
+    fn draw_field_log(ui: &mut egui::Ui, login_page: &mut LoginPage, tx: &std::sync::mpsc::Sender<String>) {
         ui.vertical_centered(|ui| {
             ui.add_space(250.0);
 
@@ -125,8 +129,7 @@ impl MyTap {
             ui.add_space(42.0);
             ui.scope(|ui| {
                 if ui.button("Login").clicked() {
-                    login_page
-                        .tx_outgoing
+                        tx
                         .send(format!("CONNECT {}", login_page.username))
                         .unwrap();
                     login_page.waiting_res = true;
@@ -164,12 +167,11 @@ impl eframe::App for MyTap {
 				egui::Image::new(image_log_bg).paint_at(ui, get_rect_screen);
                 match &mut self.screen {
                     Screen::LoginView(login_page) => {
-                        Self::draw_field_log(ui, login_page);
+                        Self::draw_field_log(ui, login_page, &self.tx_outgoing.clone());
                     }
                     Screen::GameView(game_screen) => {
-						let tx_quest = game_screen.tx_outgoing.clone();
-						game_screen.button_mod.draw_click_game(ui, &tx_quest);
                         game_screen.draw_room(ui);
+						game_screen.button_mod.draw_click_game(ui, &self.tx_outgoing);
 					}
                 };
             });
@@ -180,19 +182,18 @@ impl eframe::App for MyTap {
             login_page.toasts.show(ctx);
 
             if login_page.waiting_res {
-                match login_page.rx_incoming.try_recv() {
+                match self.rx_incoming.try_recv() {
                     Ok(ServerMessage::Ok(_)) => {
                         login_page.waiting_res = false;
                         login_page.toasts.success("Login successful".to_string());
-                        transition = Some(Screen::GameView(GameScreen {
-                            // username: login_page.username.clone(),
-							current_room: StateRoom::Room1, // replace with state room
-							tx_outgoing: login_page.tx_outgoing.clone(),
+                        self.tx_outgoing.send("LOOK".to_string()).unwrap();
+						transition = Some(Screen::GameView(GameScreen {
+							current_room: StateRoom::Room1, // default room
 							button_mod: ComandeButton::macthing_action(),
 						}));
                     }
                     Ok(ServerMessage::Err { code: 500, message }) => {
-                        login_page.toasts.error(message.clone());
+                        login_page.toasts.error(message);
                         login_page.waiting_res = false;
                     }
                     _ => {}
@@ -200,10 +201,22 @@ impl eframe::App for MyTap {
             }
         }
 		if let Screen::GameView(game_screen) = &mut self.screen {
-
+				while let Ok(msg) = self.rx_incoming.try_recv() {
+					match msg {
+						ServerMessage::Ok(reponse) => {
+							if reponse.contains("loc.tavern") {
+								game_screen.current_room = StateRoom::Room1;
+							}
+							else if reponse.contains("loc.square") {
+								game_screen.current_room = StateRoom::Room2;
+							}
+						}
+						_ => {}
+					}
+				}
+			}
+			if let Some(new_screen) = transition {
+				self.screen = new_screen;
+			}
 		}
-        if let Some(new_screen) = transition {
-            self.screen = new_screen;
-        }
-    }
 }
