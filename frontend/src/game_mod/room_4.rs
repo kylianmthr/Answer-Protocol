1302db4fe3ff66a8657492aa82625bf424ccb748
theosykas <theosykas@icloud.com>
2026-06-15 22:09:18 +0200

    refont_des

---
+++ frontend/src/game_mod/room_4.rs
@@ -0,0 +1,8 @@
+use eframe::egui;
+
+pub fn draw(ui: &mut egui::Ui) {
+	let room_asset = egui::include_image!("room_asset/ROOM_4_v2.jpg");
+	let rect = ui.max_rect();
+
+	egui::Image::new(room_asset).paint_at(ui, rect);
+}
