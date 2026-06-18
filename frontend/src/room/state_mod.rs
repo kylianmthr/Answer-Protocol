6a2d36fec0df9e3686f3ea5d3ba121259ec42201
theosykas <theosykas@icloud.com>
2026-06-18 13:33:07 +0200

    pos_Vec

--- frontend/src/room/state_mod.rs
+++ frontend/src/room/state_mod.rs
@@ -6,6 +6,10 @@ use crate::room:: {
 	room_2,
 	room_3,
 	room_4,
+	room_5,
+	room_6,
+	room_7,
+	room_8,
 };
 
 pub enum StateRoom {
@@ -13,6 +17,10 @@ pub enum StateRoom {
 	Room2,
 	Room3,
 	Room4,
+	Room5,
+	Room6,
+	Room7,
+	Room8,
 }
 
 pub struct GameScreen {
@@ -27,6 +35,33 @@ impl GameScreen {
 			StateRoom::Room2 => room_2::draw(ui),
 			StateRoom::Room3 => room_3::draw(ui),
 			StateRoom::Room4 => room_4::draw(ui),
+			StateRoom::Room5 => room_5::draw(ui),
+			StateRoom::Room6 => room_6::draw(ui),
+			StateRoom::Room7 => room_7::draw(ui),
+			StateRoom::Room8 => room_8::draw(ui),
 		}
 	}
 }
+
+
+// Départ : Tu apparais dans Le Chaudron Baveur (loc.tavern).
+
+// Tape south : Tu arrives sur la Place du Vieux Grimoire (loc.square). C'est le carrefour central de ton monde.
+
+// Tape east : Tu visites la boutique Baguettes & Sortilèges (loc.shop).
+
+// Tape west : Retour sur la Place (loc.square).
+
+// Tape west : Tu entres dans la Bibliothèque Interdite (loc.library).
+
+// Tape north : Tu montes dans la Tour de l'Astral (loc.observatory).
+
+// Tape south : Retour dans la Bibliothèque (loc.library).
+
+// Tape east : Retour sur la Place (loc.square).
+
+// Tape south : Tu t'enfonces dans la Forêt des Murmures (loc.forest).
+
+// Tape east : Tu découvres le Marais Putride (loc.swamp).
+
+// Tape south : Tu atteins la dernière zone, la Crypte des Anciens (loc.crypt), où se trouve ton PNJ hostile (Malakor).
